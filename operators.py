e8050054ea5986d4aa1da5d95a2bec4338c74f07
YesimOguz <yessoguzzz@gmail.com>
2021-05-31 23:37:17 +0300

    first commit

---
+++ operators.py
@@ -0,0 +1,17 @@
+#get two numbers from user
+number_1 = int(input('please write a number: '))
+number_2 = int(input('please write second number: '))
+
+#multiplying two numbers
+product=number_1*number_2
+print(product)
+print(f'multiplying {number_1} and {number_2} is {product}')
+
+#get two numbers from user
+num1 = int(input('please enter a number: '))
+num2 = int(input('please enter second number: '))
+
+#summation two numbers
+sum = num1+num2
+print(product)
+print(f'summation {num1} and {num2} is {product}')
